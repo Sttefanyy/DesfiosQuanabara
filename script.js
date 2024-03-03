3993adf177c70ddc8d4b05e72ba5f203fded0484
Stefany Matos <153469760+Sttefanyy@users.noreply.github.com>
2024-03-03 23:00:22 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,195 @@
+function trocaInicio() {
+  var desafio1Section = document.getElementById('desafio1');
+  var desafio2Section = document.getElementById('desafio2');
+  var desafio3Section = document.getElementById('desafio3');
+  var desafio4Section = document.getElementById('desafio4');
+  var desafio5Section = document.getElementById('desafio5');
+  var desafio6Section = document.getElementById('desafio6');
+  var desafio7Section = document.getElementById('desafio7');
+  var desafio8Section = document.getElementById('desafio8');
+  var desafio9Section = document.getElementById('desafio9');
+   var inicio = document.getElementById('inicio');
+  
+    
+    inicio.style.display = "block";
+    desafio1Section.style.display = 'none';
+    desafio2Section.style.display = 'none';
+    desafio3Section.style.display = 'none';
+    desafio4Section.style.display = 'none';
+    desafio5Section.style.display = 'none';
+    desafio6Section.style.display = 'none';
+    desafio7Section.style.display = 'none';
+    desafio8Section.style.display = 'none';
+    desafio9Section.style.display = 'none';
+}
+
+
+function trocaDesafio1() {
+  var desafio1Section = document.getElementById('desafio1');
+  var desafio2Section = document.getElementById('desafio2');
+  var desafio3Section = document.getElementById('desafio3');
+  var desafio4Section = document.getElementById('desafio4');
+  var desafio5Section = document.getElementById('desafio5');
+  var desafio6Section = document.getElementById('desafio6');
+  var desafio7Section = document.getElementById('desafio7');
+  var desafio8Section = document.getElementById('desafio8');
+  var desafio9Section = document.getElementById('desafio9');
+  var inicio = document.getElementById('inicio');
+
+    inicio.style.display = "none";
+    desafio1Section.style.display = 'block';
+    desafio2Section.style.display = 'none';
+    desafio3Section.style.display = 'none';
+    desafio4Section.style.display = 'none';
+    desafio5Section.style.display = 'none';
+    desafio6Section.style.display = 'none';
+    desafio7Section.style.display = 'none';
+    desafio8Section.style.display = 'none';
+    desafio9Section.style.display = 'none';
+}
+
+
+function trocaDesafio2() {
+  var desafio1Section = document.getElementById('desafio1');
+  var desafio2Section = document.getElementById('desafio2');
+  var desafio3Section = document.getElementById('desafio3');
+  var desafio4Section = document.getElementById('desafio4');
+  var desafio5Section = document.getElementById('desafio5');
+  var desafio6Section = document.getElementById('desafio6');
+  var desafio7Section = document.getElementById('desafio7');
+  var desafio8Section = document.getElementById('desafio8');
+  var desafio9Section = document.getElementById('desafio9');
+  var inicio = document.getElementById('inicio');
+
+    inicio.style.display = "none";
+    desafio1Section.style.display = 'none';
+    desafio2Section.style.display = 'block';
+    desafio3Section.style.display = 'none';
+    desafio4Section.style.display = 'none';
+    desafio5Section.style.display = 'none';
+    desafio6Section.style.display = 'none';
+    desafio7Section.style.display = 'none';
+    desafio8Section.style.display = 'none';
+    desafio9Section.style.display = 'none';
+  
+}
+
+function trocaDesafio3() {
+  var desafio1Section = document.getElementById('desafio1');
+  var desafio2Section = document.getElementById('desafio2');
+  var desafio3Section = document.getElementById('desafio3');
+  var desafio4Section = document.getElementById('desafio4');
+  var desafio5Section = document.getElementById('desafio5');
+  var desafio6Section = document.getElementById('desafio6');
+  var desafio7Section = document.getElementById('desafio7');
+  var desafio8Section = document.getElementById('desafio8');
+  var desafio9Section = document.getElementById('desafio9');
+  var inicio = document.getElementById('inicio');
+
+    inicio.style.display = "none";
+    desafio1Section.style.display = 'none';
+    desafio2Section.style.display = 'none';
+    desafio3Section.style.display = 'block';
+    desafio4Section.style.display = 'none';
+    desafio5Section.style.display = 'none';
+    desafio6Section.style.display = 'none';
+    desafio7Section.style.display = 'none';
+    desafio8Section.style.display = 'none';
+    desafio9Section.style.display = 'none';
+}
+
+function trocaDesafio4() {
+  var desafio1Section = document.getElementById('desafio1');
+  var desafio2Section = document.getElementById('desafio2');
+  var desafio3Section = document.getElementById('desafio3');
+  var desafio4Section = document.getElementById('desafio4');
+  var desafio5Section = document.getElementById('desafio5');
+  var desafio6Section = document.getElementById('desafio6');
+  var desafio7Section = document.getElementById('desafio7');
+  var desafio8Section = document.getElementById('desafio8');
+  var desafio9Section = document.getElementById('desafio9');
+  var inicio = document.getElementById('inicio');
+
+    inicio.style.display = "none";
+    desafio1Section.style.display = 'none';
+    desafio2Section.style.display = 'none';
+    desafio3Section.style.display = 'none';
+    desafio4Section.style.display = 'block';
+    desafio5Section.style.display = 'none';
+    desafio6Section.style.display = 'none';
+    desafio7Section.style.display = 'none';
+    desafio8Section.style.display = 'none';
+    desafio9Section.style.display = 'none';
+}
+
+function trocaDesafio5() {
+  var desafio1Section = document.getElementById('desafio1');
+  var desafio2Section = document.getElementById('desafio2');
+  var desafio3Section = document.getElementById('desafio3');
+  var desafio4Section = document.getElementById('desafio4');
+  var desafio5Section = document.getElementById('desafio5');
+  var desafio6Section = document.getElementById('desafio6');
+  var desafio7Section = document.getElementById('desafio7');
+  var desafio8Section = document.getElementById('desafio8');
+  var desafio9Section = document.getElementById('desafio9');
+  var inicio = document.getElementById('inicio');
+
+    inicio.style.display = "none";
+    desafio1Section.style.display = 'none';
+    desafio2Section.style.display = 'none';
+    desafio3Section.style.display = 'none';
+    desafio4Section.style.display = 'none';
+    desafio5Section.style.display = 'block';
+    desafio6Section.style.display = 'none';
+    desafio7Section.style.display = 'none';
+    desafio8Section.style.display = 'none';
+    desafio9Section.style.display = 'none';
+}
+
+function trocaDesafio6 () {
+  var desafio1Section = document.getElementById('desafio1');
+  var desafio2Section = document.getElementById('desafio2');
+  var desafio3Section = document.getElementById('desafio3');
+  var desafio4Section = document.getElementById('desafio4');
+  var desafio5Section = document.getElementById('desafio5');
+  var desafio6Section = document.getElementById('desafio6');
+  var desafio7Section = document.getElementById('desafio7');
+  var desafio8Section = document.getElementById('desafio8');
+  var desafio9Section = document.getElementById('desafio9');
+  var inicio = document.getElementById('inicio');
+
+    inicio.style.display = "none";
+    desafio1Section.style.display = 'none';
+    desafio2Section.style.display = 'none';
+    desafio3Section.style.display = 'none';
+    desafio4Section.style.display = 'none';
+    desafio5Section.style.display = 'none';
+    desafio6Section.style.display = 'block';
+    desafio7Section.style.display = 'none';
+    desafio8Section.style.display = 'none';
+    desafio9Section.style.display = 'none';
+}
+
+function trocaDesafio7 () {
+  var desafio1Section = document.getElementById('desafio1');
+  var desafio2Section = document.getElementById('desafio2');
+  var desafio3Section = document.getElementById('desafio3');
+  var desafio4Section = document.getElementById('desafio4');
+  var desafio5Section = document.getElementById('desafio5');
+  var desafio6Section = document.getElementById('desafio6');
+  var desafio7Section = document.getElementById('desafio7');
+  var desafio8Section = document.getElementById('desafio8');
+  var desafio9Section = document.getElementById('desafio9');
+  var inicio = document.getElementById('inicio');
+
+    inicio.style.display = "none";
+    desafio1Section.style.display = 'none';
+    desafio2Section.style.display = 'none';
+    desafio3Section.style.display = 'none';
+    desafio4Section.style.display = 'none';
+    desafio5Section.style.display = 'none';
+    desafio6Section.style.display = 'none';
+    desafio7Section.style.display = 'block';
+    desafio8Section.style.display = 'none';
+    desafio9Section.style.display = 'none';
+}
